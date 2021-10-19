26d32ba038885307bb78c2fa47b0cb62874bfeb0
Emon Ahmed <technicalboyemon@gmail.com>
2021-10-19 14:21:03 +0600

    Files Added

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './components.css'
+
+export default function header() {
+    return (
+        <div>
+            <div className="container">
+                Hello
+            </div>
+        </div>
+    )
+}
