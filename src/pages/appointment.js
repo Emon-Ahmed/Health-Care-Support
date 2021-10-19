e09e7ca4d05a674d49e053f05435cc23a172ba59
Emon Ahmed <technicalboyemon@gmail.com>
2021-10-19 21:01:07 +0600

    Files

--- src/pages/appointment.js
+++ src/pages/appointment.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
 
 export default function appointment() {
-    return (
-        <div>
-            
+  return (
+    <div className="container">
+      <div className="contact-area">
+        <div className="contact-text">
+          <h1>Appointment</h1>
+          <div className="contact-form">
+          <div class="mb-3">
+              <label for="exampleFormControlInput1" class="form-label">
+                Your Name
+              </label>
+              <input
+                type="text"
+                class="form-control"
+                id="exampleFormControlInput1"
+                placeholder="Your Name"
+              />
+            </div>
+            <div class="mb-3">
+              <label for="exampleFormControlInput1" class="form-label">
+                Your Problem
+              </label>
+              <input
+                type="text"
+                class="form-control"
+                id="exampleFormControlInput1"
+                placeholder="Your Problem"
+              />
+            </div>
+            <div class="mb-3">
+              <label for="exampleFormControlInput1" class="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                class="form-control"
+                id="exampleFormControlInput1"
+                placeholder="name@example.com"
+              />
+            </div>
+            <button className="btn nav-fancy">Registration</button>
+          </div>
         </div>
-    )
+        <div className="contact-img">
+          <img src="banner.svg" alt="contact"></img>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,15 +8,13 @@ export default function contact() {
         <div className="contact-text">
           <h1>Contact Us</h1>
           <div className="contact-form">
-            <div>
-              <div className="mb-3 input-from">
-                <label>Email Address</label>
-                <input type="email" placeholder="name@example.com" />
-              </div>
-              <div className="mb-3 input-from">
-                <label>Your Message</label>
-                <textarea  placeholder="Your Message" />
-              </div>
+            <div class="mb-3">
+              <label for="exampleFormControlInput1" class="form-label">Email address</label>
+              <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+            </div>
+            <div class="mb-3">
+              <label for="exampleFormControlTextarea1" class="form-label">Your Message</label>
+              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
             </div>
             <button className="btn nav-fancy">Send Message</button>
           </div>
